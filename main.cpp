6f02b083d0c3ec9add9d20e388d15f8636f14506
Anıl Taha <anil.taha@hotmail.com>
2015-03-25 23:16:55 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"libra.h"
+#include<string>
+using namespace std;
+string plaka(int rakam);
+int main()
+{
+int rakam;//
+  string sehir;
+    cin>>rakam;
+      sehir=plaka(rakam);
+        cout<<sehir;
+
+}
